ab6690445f10bf56eacf2641838927a2f595cc6a
root <root@Chaitanya-Raj.localdomain>
2026-05-13 16:46:27 +0000

    21th commit - Acer Updates - Terraform-BACKEND-Concept

--- DevOPS/Activity/terraform-practice/22_March_Files/resource_grp.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "RG1" {
-  name     = "test-RG1"
-  location = "West India"
-}
--- DevOPS/Activity/terraform-practice/create_rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "RG27March" {
-  name     = "test_new_rg"
-  location = "West Europe"
-}
